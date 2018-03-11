7f814819a56b9cd10e892f5cca8fac021dc80ac3
Administrator <Administrator@YLMF-1704081533>
2018-03-11 17:00:33 +0800

    添加注释

--- src/main/java/com/example/demo/ConfigApplication.java
+++ src/main/java/com/example/demo/ConfigApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+/**
+ * 配置服务中心 ：使用git远程配置仓库
+ * @author mhs123
+ *
+ */
 @EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
